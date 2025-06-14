ff998af07c374c05a95c40a2ee33d3c30c2f94b9
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2025-06-14 08:20:55 -0400

    Added Custom Exception for File Reading and Enum type for Job handling switch case in BatchConfig class

---
+++ src/main/java/edu/charlotte/parser/config/JobType.java
@@ -0,0 +1,28 @@
+package edu.charlotte.parser.config;
+
+import edu.charlotte.parser.utils.Constants;
+import lombok.Getter;
+
+@Getter
+public enum JobType {
+    DL_AST_GENERATION(Constants.JOBNAME_DL_AST_GENERATION, Constants.AST_GENERATION_EXTENSION),
+    DL_TO_KEYMAERAX_OUTPUT_CONVERSION(Constants.JOBNAME_DL_TO_KEYMAERAX_OUTPUT_CONVERSION, Constants.KEYMAERAX_EXTENSION),
+    REL_DL_AST_GENERATION(Constants.JOBNAME_REL_DL_AST_GENERATION, Constants.AST_GENERATION_EXTENSION);
+
+    private final String jobNameIdentifier;
+    private final String fileExtension;
+
+    JobType(String jobNameIdentifier, String fileExtension) {
+        this.jobNameIdentifier = jobNameIdentifier;
+        this.fileExtension = fileExtension;
+    }
+
+    // Helper method to get JobType from the Job Name identifier string
+    public static JobType getJobType(String name) {
+        for (JobType type : JobType.values()) {
+            if (type.getJobNameIdentifier().equalsIgnoreCase(name))
+                return type;
+        }
+        throw new IllegalArgumentException("Invalid Job: " + name);
+    }
+}
