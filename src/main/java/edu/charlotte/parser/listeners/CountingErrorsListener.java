d575285a5702b66bd777c60621314766b5404dd0
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2025-06-20 20:05:08 -0400

    Making changes according to Sonar best practices and understanding best practices using AI and Internet

--- src/main/java/edu/charlotte/parser/listeners/CountingErrorsListener.java
+++ src/main/java/edu/charlotte/parser/listeners/CountingErrorsListener.java
@@ -7,21 +7,24 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Slf4j
 public class CountingErrorsListener extends BaseErrorListener {
     private int errorCount = 0;
-    private final List<String> errorMessages = new ArrayList<>();
+
+    public CountingErrorsListener() {
+        log.debug("Instance of CountingErrorsListener is created.");
+    }
+
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
                             String msg, RecognitionException e) {
-        String errorMsg = (recognizer instanceof Lexer ? "Lexer" : "Parser") +
-                " error at line " + line + ":" + charPositionInLine + " - " + msg;
-        errorMessages.add(errorMsg);
-        log.error("{}", errorMsg);
+        String errorSource = (recognizer instanceof Lexer) ? "Lexer" : "Parser";
+        String errorMsg = String.format("%s error at line %d: %d - %s", errorSource, line, charPositionInLine, msg);
+        if(e != null)
+            log.error("{}", errorMsg, e);
+        else
+            log.error("{}", errorMsg);
         errorCount++;
     }
 }
--- src/main/java/edu/charlotte/parser/listeners/JobLoggingListener.java
+++ src/main/java/edu/charlotte/parser/listeners/JobLoggingListener.java
@@ -1,19 +1,40 @@
 package edu.charlotte.parser.listeners;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 
 @Slf4j
 public class JobLoggingListener implements JobExecutionListener {
 
+    public JobLoggingListener() {
+        log.debug("JobLoggingListener instance is created.");
+    }
+
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        log.info("Job {} started at: {}", jobExecution.getJobInstance().getJobName(), jobExecution.getStartTime());
+        log.info("Job '{}' (ID: {}) started at: {}", jobExecution.getJobInstance().getJobName(),
+                jobExecution.getJobId(), jobExecution.getStartTime());
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        log.info("Job {} completed with status: {} at {}", jobExecution.getJobInstance().getJobName(), jobExecution.getStatus(), jobExecution.getEndTime());
+        log.info("Job '{}' (ID: {}) completed with status: {} at {}",
+                jobExecution.getJobInstance().getJobName(), jobExecution.getJobId(),
+                jobExecution.getStatus(), jobExecution.getEndTime());
+
+        if (jobExecution.getStatus() == BatchStatus.FAILED) {
+            log.error("Job '{}' Failed. Job Exit Description is: {}",
+                    jobExecution.getJobInstance().getJobName(),
+                    jobExecution.getExitStatus().getExitDescription());
+
+            if (!jobExecution.getAllFailureExceptions().isEmpty()) {
+                log.error("Job '{}' failure exceptions are:", jobExecution.getJobInstance().getJobName());
+                for (Throwable throwable : jobExecution.getAllFailureExceptions()) {
+                    log.error("\n\t{}", throwable.getMessage(), throwable);
+                }
+            }
+        }
     }
 }
