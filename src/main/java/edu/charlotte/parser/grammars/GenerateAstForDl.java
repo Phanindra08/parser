58b3df0145b1b3b15014b681f17e5312ab96eeb1
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2025-06-26 19:59:31 -0400

    Creating the code for generating AST for Rel DL and improvements to the code for Generating AST for DL

--- src/main/java/edu/charlotte/parser/grammars/GenerateAstForDl.java
+++ src/main/java/edu/charlotte/parser/grammars/GenerateAstForDl.java
@@ -1,14 +1,14 @@
-package edu.charlotte.parser.parser_for_grammars;
+package edu.charlotte.parser.grammars;
 
-import edu.charlotte.parser.antlr4_parser.grammar.dynamic_differential_logic.DynamicDifferentialLogicLexer;
-import edu.charlotte.parser.antlr4_parser.grammar.dynamic_differential_logic.DynamicDifferentialLogicParser;
+import edu.charlotte.parser.dynamic_differential_logic.DynamicDifferentialLogicLexer;
+import edu.charlotte.parser.dynamic_differential_logic.DynamicDifferentialLogicParser;
 import edu.charlotte.parser.listeners.ast.DlAstListener;
 import edu.charlotte.parser.utils.Constants;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class GenerateASTForDL extends AbstractASTGenerator<DynamicDifferentialLogicLexer,
+public class GenerateAstForDl extends AbstractAstGenerator<DynamicDifferentialLogicLexer,
         DynamicDifferentialLogicParser, DlAstListener> {
 
     @Override
---
+++ src/main/java/edu/charlotte/parser/grammars/GenerateAstForRelDl.java
@@ -0,0 +1,47 @@
+package edu.charlotte.parser.grammars;
+
+import edu.charlotte.parser.listeners.ast.RelDlAstListener;
+import edu.charlotte.parser.relational_dynamic_logic.RelationalDynamicLogicLexer;
+import edu.charlotte.parser.relational_dynamic_logic.RelationalDynamicLogicParser;
+import edu.charlotte.parser.utils.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+@Slf4j
+public class GenerateAstForRelDl extends AbstractAstGenerator<RelationalDynamicLogicLexer,
+        RelationalDynamicLogicParser, RelDlAstListener> {
+
+    private final boolean hasKeYMaeraXConversion;
+
+    public GenerateAstForRelDl(boolean hasKeYMaeraXConversion) {
+        super();
+        this.hasKeYMaeraXConversion = hasKeYMaeraXConversion;
+    }
+
+    @Override
+    protected RelationalDynamicLogicLexer createLexerInstance(CharStream input) {
+        return new RelationalDynamicLogicLexer(input);
+    }
+
+    @Override
+    protected RelationalDynamicLogicParser createParserInstance(CommonTokenStream tokens) {
+        return new RelationalDynamicLogicParser(tokens);
+    }
+
+    @Override
+    protected ParseTree invokeTopLevelParseRule(RelationalDynamicLogicParser parser) {
+        return parser.relDlProgram();
+    }
+
+    @Override
+    protected RelDlAstListener createAstListenerInstance() {
+        return new RelDlAstListener(this.hasKeYMaeraXConversion);
+    }
+
+    @Override
+    public String getTypeName() {
+        return Constants.RELATIONAL_DYNAMIC_LOGIC;
+    }
+}
