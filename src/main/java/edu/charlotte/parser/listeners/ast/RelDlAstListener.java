ed919965c6a8cecba59869b7251ef8aef4ad1636
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2025-06-27 11:25:32 -0400

    Changing Relational DL AST Listener class based on Sonar Best practices and understanding best practices using AI and Internet

--- src/main/java/edu/charlotte/parser/listeners/ast/RelDlAstListener.java
+++ src/main/java/edu/charlotte/parser/listeners/ast/RelDlAstListener.java
@@ -1,8 +1,8 @@
-package edu.charlotte.parser.listeners.ast_listeners;
+package edu.charlotte.parser.listeners.ast;
 
-import edu.charlotte.parser.antlr4_parser.grammar.relational_dynamic_logic.RelationalDynamicLogicBaseListener;
-import edu.charlotte.parser.antlr4_parser.grammar.relational_dynamic_logic.RelationalDynamicLogicParser;
 import edu.charlotte.parser.ast.nodes.AstNode;
+import edu.charlotte.parser.relational_dynamic_logic.RelationalDynamicLogicBaseListener;
+import edu.charlotte.parser.relational_dynamic_logic.RelationalDynamicLogicParser;
 import edu.charlotte.parser.utils.AstListenerUtils;
 import edu.charlotte.parser.utils.Constants;
 import lombok.extern.slf4j.Slf4j;
@@ -14,27 +14,30 @@ import java.util.*;
 public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
     private final Stack<AstNode> stack;
     private final Map<Character, Set<String>> identifiers;
+    private final boolean hasKeYMaeraXConversion;
     private char programConsidered;
 
-    public RelDlAstListener() {
+    public RelDlAstListener(boolean hasKeYMaeraXConversion) {
         this.stack = new Stack<>();
         this.identifiers = new HashMap<>();
         this.identifiers.put(Constants.PROGRAM_CONSIDERED_L, new HashSet<>());
         this.identifiers.put(Constants.PROGRAM_CONSIDERED_R, new HashSet<>());
         this.identifiers.put(Constants.PROGRAM_CONSIDERED_G, new HashSet<>());
         this.programConsidered = Constants.PROGRAM_CONSIDERED_G;
-        log.debug("RelDlAstListener is initialized with the default program considered is '{}'.", this.programConsidered);
+        this.hasKeYMaeraXConversion = hasKeYMaeraXConversion;
+        log.debug("RelDlAstListener is initialized with the default program considered is '{}'. KeYMaeraX conversion enabled: {}.",
+                this.programConsidered, this.hasKeYMaeraXConversion);
     }
 
     // Relation DL Program (root of the file)
     @Override
-    public void enterRelDLProgram(RelationalDynamicLogicParser.RelDLProgramContext ctx) {
+    public void enterRelDlProgram(RelationalDynamicLogicParser.RelDlProgramContext ctx) {
         log.debug("Entering Relational DL-Program rule: {}", ctx.getText());
         stack.push(new AstNode(Constants.AST_NODE_REL_DL_PROGRAM));
     }
 
     @Override
-    public void exitRelDLProgram(RelationalDynamicLogicParser.RelDLProgramContext ctx) {
+    public void exitRelDlProgram(RelationalDynamicLogicParser.RelDlProgramContext ctx) {
         log.debug("Exiting Relational DL-Program rule: {}", ctx.getText());
         List<AstNode> childNodes = AstListenerUtils.exitGrammarRule(ctx, stack);
         AstListenerUtils.addChildrenToLastNodeInStack(childNodes, Constants.AST_NODE_REL_DL_PROGRAM, ctx.getText(), stack);
@@ -50,7 +53,7 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
     public void exitRelProgram(RelationalDynamicLogicParser.RelProgramContext ctx) {
         log.debug("Exiting Relational program context rule: {}", ctx.getText());
         List<AstNode> childNodes = AstListenerUtils.exitGrammarRule(ctx, stack);
-        if(ctx.REL_TERNARY_OPERATOR() != null || ctx.REL_ASSIGNMENT_OPERATOR() != null) {
+        if(hasKeYMaeraXConversion && (ctx.REL_DL_TERNARY_OPERATOR() != null || ctx.REL_DL_ASSIGNMENT_OPERATOR() != null)) {
             log.info("The Relational program context contains a ternary or assignment operator. " +
                     "Adding the ';' symbol as a child node to the AST Node List.");
             childNodes.add(new AstNode(";"));
@@ -76,11 +79,11 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
         log.debug("Entering Relational term rule: {}", ctx.getText());
         stack.push(new AstNode(Constants.AST_NODE_REL_DL_TERM));
         if(ctx.PROGRAM_CONSIDERED().getText().equals(Constants.LEFT_PROGRAM))
-            this.programConsidered = 'L';
+            this.programConsidered = Constants.PROGRAM_CONSIDERED_L;
         else if(ctx.PROGRAM_CONSIDERED().getText().equals(Constants.RIGHT_PROGRAM))
-            this.programConsidered = 'R';
+            this.programConsidered = Constants.PROGRAM_CONSIDERED_R;
         else
-            this.programConsidered = 'G';
+            this.programConsidered = Constants.PROGRAM_CONSIDERED_G;
         log.info("Program considered is set to '{}' for current RelTerm context.", this.programConsidered);
     }
 
@@ -90,7 +93,7 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
         List<AstNode> childNodes = AstListenerUtils.exitGrammarRule(ctx, stack);
         AstListenerUtils.addChildrenToLastNodeInStack(childNodes, Constants.AST_NODE_REL_DL_TERM, ctx.getText(), stack);
 
-        this.programConsidered = 'G';
+        this.programConsidered = Constants.PROGRAM_CONSIDERED_G;
         log.info("Program considered is set to the default value '{}' after exiting RelTerm.", this.programConsidered);
     }
 
@@ -161,6 +164,19 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
     }
 
     @Override
+    public void enterParenthesesTerm(RelationalDynamicLogicParser.ParenthesesTermContext ctx) {
+        log.debug("Entering Parentheses Term rule '{}' within Relational DL.", ctx.getText());
+        stack.push(new AstNode(Constants.AST_NODE_DL_TERM_WITH_PARENTHESES));
+    }
+
+    @Override
+    public void exitParenthesesTerm(RelationalDynamicLogicParser.ParenthesesTermContext ctx) {
+        log.debug("Exiting Parentheses Term rule: {}", ctx.getText());
+        List<AstNode> childNodes = AstListenerUtils.exitGrammarRule(ctx, stack);
+        AstListenerUtils.addChildrenToLastNodeInStack(childNodes, Constants.AST_NODE_DL_TERM_WITH_PARENTHESES, ctx.getText(), stack);
+    }
+
+    @Override
     public void visitTerminal(TerminalNode node) {
         log.debug("Visiting terminal '{}' within Relational DL.", node.getText());
         stack.push(new AstNode(node.getText()));
@@ -183,8 +199,8 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
         log.debug("Added identifier '{}' to the program considered '{}'.", identifier, this.programConsidered);
     }
 
-    // Return the final AST root node
-    public AstNode getAST() {
+    // Return the final Ast root node
+    public AstNode getAst() {
         if(stack.size() > 1)
             log.warn("Stack contains more than one element after AST generation. The might be a possible issue in listener logic. " +
                     "Stack size is: {} and the contents are: {}", stack.size(), stack);
@@ -195,7 +211,7 @@ public class RelDlAstListener extends RelationalDynamicLogicBaseListener {
     public Map<Character, Set<String>> getIdentifiers() {
         Map<Character, Set<String>> unmodifiableMap = new HashMap<>();
         for (Map.Entry<Character, Set<String>> entry : this.identifiers.entrySet()) {
-            unmodifiableMap.put(entry.getKey(), Collections.unmodifiableSet(entry.getValue()));
+            unmodifiableMap.put(entry.getKey(), Collections.unmodifiableSet(Objects.requireNonNullElse(entry.getValue(), Collections.emptySet())));
         }
         return Collections.unmodifiableMap(unmodifiableMap);
     }
